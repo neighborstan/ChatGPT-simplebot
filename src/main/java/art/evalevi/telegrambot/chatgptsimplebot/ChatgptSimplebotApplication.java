2921eb6b15513d88e60da69a210007cf3e32dc12
neighborstan <dropweb.post@gmail.com>
2023-06-04 15:19:11 +0600

    Init a Spring Boot Project

---
+++ src/main/java/art/evalevi/telegrambot/chatgptsimplebot/ChatgptSimplebotApplication.java
@@ -0,0 +1,13 @@
+package art.evalevi.telegrambot.chatgptsimplebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptSimplebotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatgptSimplebotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/art/evalevi/telegrambot/chatgptsimplebot/ChatgptSimplebotApplicationTests.java
@@ -0,0 +1,13 @@
+package art.evalevi.telegrambot.chatgptsimplebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgptSimplebotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
